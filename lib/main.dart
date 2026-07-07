2ccd6d5a20fe92ddbd458600f91083749dcf9534
zlorgoncho1 <papamatardiop3@gmail.com>
2026-07-07 19:08:26 +0000

    Modification de la couleur de l'application et du titre

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.green),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Mon Compteur'),
     );
   }
 }
